fdd29956a623d99c1fca64fcc3b711af7761e0d0
joaodosanjos7 <anjosjaoxabj@gmail.com>
2026-06-12 10:25:49 -0300

    Completo
    Completo

--- 1_POO vs Estruturada/JS/poo.js
+++ 1_POO vs Estruturada/JS/poo.js
@@ -2,6 +2,56 @@
 // PARTE 2: PROGRAMAÇÃO ORIENTADA A OBJETOS EM JS
 // ==========================================
 
+class Cachorro {
+
+    // # Atributos privados (Encapsulado)
+    // ou seja não permite acesso direto
+    #nome
+    #comida
+    #sono
+
+    // Construtor (executado automaticamente)
+    // quando criamos um novo objeto
+    // Ele recebe valores iniciais e salva
+    // this é como dizer "meu próprio atributo"
+    
+    constructor(nome, comida, sono) {
+        this.#nome = nome
+        this.#comida = comida
+        this.#sono = sono
+    }
+
+    // Metodo comer
+    comer() {
+        this.#comida += 1
+    }
+    
+    // Metodo dormir
+    dormir() {
+        this.#sono = true
+    }
+    
+    // Getters para acessar os atributos privados
+    getNome() {
+        return this.#nome
+    }
+
+    getComida() {
+        return this.#comida
+    }
+
+    getSono() {
+        return this.#sono
+    }
+}
+
+//Criando instancias ou objetos
+const cachorro1 = new Cachorro("Bob", 5, false)
+const cachorro2 = new Cachorro("Snoop", 8, true)
+
+// Usando os métodos
+cachorro1.comer()
+cachorro2.dormir()
 
 
 // Digitar o código (Feito pelo Professor - AQUI)
